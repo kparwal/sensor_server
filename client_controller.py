97169a74c1ea36b1ae3d97d56a562c4621c96183
Keshav Parwal <kparwal260@gmail.com>
2016-09-30 15:35:47 -0400

    updated client_controller to have min max functionality

--- client_controller.py
+++ client_controller.py
@@ -1,10 +1,17 @@
+import sys
 class sensor_client:
     def __init__(self, username, password):
         self.running_count = 0
         self.running_sum = 0
+        self.max_temp = sys.maxint * -1 + 1
+        self.min_temp = sys.maxint
         self.username = username
         self.password = password
     def update(self, temp):
+        if (temp > self.max_temp):
+            self.max_temp = temp
+        if (temp < self.min_temp):
+            self.min_temp = temp
         self.running_count+= 1
         self.running_sum += temp
     def average(self):
